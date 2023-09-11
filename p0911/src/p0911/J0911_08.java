6e470472940c970f25d5e9d50f06407f911a8035
KOREAVC Professor <KOREAVC Professor@DESKTOP-TV4UGS1>
2023-09-11 15:38:31 +0900

    9월 11일수업

---
+++ p0911/src/p0911/J0911_08.java
@@ -0,0 +1,17 @@
+package p0911;
+
+public class J0911_08 {
+
+	int result; //자동으로 0을 넣어줌. 인스턴스변수,클래스변수는 가능.
+	
+	public static void main(String[] args) {
+		
+		int result=0;
+		
+		System.out.println("결과값 : "+result);
+
+	}
+	
+	
+
+}

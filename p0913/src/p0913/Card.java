4aa15985d75c7d1c46a08b0f9181933c810c5d63
KOREAVC Professor <KOREAVC Professor@DESKTOP-TV4UGS1>
2023-09-13 18:23:28 +0900

    9월 13일 객체,다형성

--- p0913/src/p0913/Card.java
+++ p0913/src/p0913/Card.java
@@ -1,4 +1,4 @@
-package p0912;
+package p0913;
 
 public class Card {
 	
@@ -8,17 +8,16 @@ public class Card {
 		this.number = number;
 	}
 	
+	String kind; //모양 Spade,Heart,Diamond,Clover
+	int number;  //1,2,3,4,5 ..11(J),12(Q),13(K)
+	private int flag;
 	
-	final int NUMBER_MAX = 13;
-	String kind;
-	int number;
 	String[] CardShape = {"0","1","2","3","4","5","6","7",
 			"8","9","10","J","Q","k"};
 	
-	
-	//J-11,Q-12,K-13
-	
+	@Override
 	public String toString() {
-		return String.format("[ %s, %s ]", kind,CardShape[number]);
+		return String.format("[ %s,%s ]",kind, CardShape[number]);
 	}
+
 }

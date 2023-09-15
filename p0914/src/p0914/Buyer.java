aa04cbb725840851904f6052c714900340035bf9
KOREAVC Professor <KOREAVC Professor@DESKTOP-TV4UGS1>
2023-09-15 10:50:43 +0900

    다형성,ArrayList 쇼핑몰 구매

--- p0914/src/p0914/Buyer.java
+++ p0914/src/p0914/Buyer.java
@@ -18,10 +18,17 @@ public class Buyer {
 	//컬렉션 선언
 	ArrayList list = new ArrayList();
 	
-	void buy(Product p) {
+	int buy(Product p) {
+		//상품금액이 현재잔액보다 큰 경우
+		if(money<p.price) {
+			System.out.println("[알림] 잔액이 부족하여 구매할 수 없습니다.");
+			System.out.println();
+			return 0; //구매실패
+		}
 		money -= p.price;
 		bonusPoint += p.bonusPoint;
 		list.add(p);
+		return 1; //구매성공
 	}
 	
 	

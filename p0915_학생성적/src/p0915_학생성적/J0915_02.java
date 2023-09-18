b3f8376d764e2470b02c21d6fcb04974877a5c95
KOREAVC Professor <KOREAVC Professor@DESKTOP-TV4UGS1>
2023-09-18 16:19:29 +0900

    학생성적 삭제

--- p0915_학생성적/src/p0915_학생성적/J0915_02.java
+++ p0915_학생성적/src/p0915_학생성적/J0915_02.java
@@ -26,60 +26,17 @@ public class J0915_02 {
 				break;
 			case 3:
 				//3. 학생성적수정 메소드
-				System.out.println();
-				System.out.println("[ 학생성적 수정 ]");
-				System.out.printf("찾고자 하는 학생 이름을 입력하세요.(0.이전페이지 이동)\n");
-				String name = scan.next();
-				if(name.equals("0")) { //이전페이지 이동
-					System.out.println(">> 이전페이지로 이동합니다!!");
-					System.out.println();
-					break;
-				}
-				
-				//홍길동
-				int count = 0;
-				for(int i=0;i<stuAction.list.size();i++) {
-				
-					StuScore s = (StuScore) stuAction.list.get(i);
-					if(name.equals(s.getName())) {
-						count = i; //찾은 방 번호
-						System.out.printf("%s 학생을 찾았습니다.\n",name);
-						System.out.println("[ 수정과목 선택 ] ");
-						System.out.println("1. 국어");
-						System.out.println("2. 영어");
-						System.out.println("3. 수학");
-						System.out.println("------------------");
-						System.out.println("원하는 번호를 선택하세요.");
-						choice = scan.nextInt();
-						
-						switch(choice) {
-						case 1:
-							System.out.println("[ 국어과목 수정 ]");
-							System.out.println("-----------------------");
-							System.out.println("현재점수 : "+ s.getKor());
-							System.out.println("변경할 점수를 입력하세요.");
-							s.setKor(scan.nextInt()); //입력 후 바로 국어점수에 저장
-							s.setTotal(s.getKor()+s.getEng()+s.getMath()); //점수를 읽은 후 바로 합계 수정
-							s.setAvg(s.getTotal()/3.0); //평균 수정
-							System.out.println("국어점수가 변경되었습니다!");
-							System.out.println();
-							break;
-						}
-					}
-				
-				
-				}//for
-				
+				stuAction.stuUpdate();
+				break;
+			case 4:
+				//4. 학생성적삭제 메소드
 				
 				break;
-				
 				
 			}//switch
 			
 		}//while
 		
 		
-		
-
 	}//main
 }//class
--- p0915_학생성적/src/p0915_학생성적/StuAction.java
+++ p0915_학생성적/src/p0915_학생성적/StuAction.java
@@ -77,6 +77,129 @@ public class StuAction {
 		System.out.println();
 	}//stuOutput
 	
+	//학생성적수정 메소드
+	void stuUpdate() {
+		//붙여넣기
+		while(true) {
+			//3. 학생성적수정 메소드
+			System.out.println();
+			System.out.println("[ 학생성적 수정 ]");
+			System.out.printf("찾고자 하는 학생 이름을 입력하세요.(0.이전페이지 이동)\n");
+			String name = scan.next();
+			if(name.equals("0")) { //이전페이지 이동
+				System.out.println(">> 이전페이지로 이동합니다!!");
+				System.out.println();
+				break;
+			}
+			
+			int count = -1; //0,1,2......
+			for(int i=0;i<list.size();i++) {
+				StuScore s = (StuScore) list.get(i);
+				if(name.equals(s.getName())) {
+					count = i; //찾은 방 번호
+					System.out.printf("%s 학생을 찾았습니다.\n",name);
+					System.out.println("[ 수정과목 선택 ] ");
+					System.out.println("1. 국어");
+					System.out.println("2. 영어");
+					System.out.println("3. 수학");
+					System.out.println("------------------");
+					System.out.println("원하는 번호를 선택하세요.");
+					int choice = scan.nextInt();
+					
+					switch(choice) {
+					case 1:
+						System.out.println("[ 국어과목 수정 ]");
+						System.out.println("-----------------------");
+						System.out.println("현재점수 : "+ s.getKor());
+						System.out.println("변경할 점수를 입력하세요.");
+						s.setKor(scan.nextInt()); //입력 후 바로 점수에 저장
+						s.setTotal(s.getKor()+s.getEng()+s.getMath()); //점수를 읽은 후 바로 합계 수정
+						s.setAvg(s.getTotal()/3.0); //평균 수정
+						System.out.println("국어점수가 변경되었습니다!");
+						System.out.println();
+						break;
+					case 2:
+						System.out.println("[ 영어과목 수정 ]");
+						System.out.println("-----------------------");
+						System.out.println("현재점수 : "+ s.getEng());
+						System.out.println("변경할 점수를 입력하세요.");
+						s.setEng(scan.nextInt()); //입력 후 바로 점수에 저장
+						s.setTotal(s.getKor()+s.getEng()+s.getMath()); //점수를 읽은 후 바로 합계 수정
+						s.setAvg(s.getTotal()/3.0); //평균 수정
+						System.out.println("영어점수가 변경되었습니다!");
+						System.out.println();
+						break;
+					case 3:
+						System.out.println("[ 수학과목 수정 ]");
+						System.out.println("-----------------------");
+						System.out.println("현재점수 : "+ s.getMath());
+						System.out.println("변경할 점수를 입력하세요.");
+						s.setMath(scan.nextInt()); //입력 후 바로 점수에 저장
+						s.setTotal(s.getKor()+s.getEng()+s.getMath()); //점수를 읽은 후 바로 합계 수정
+						s.setAvg(s.getTotal()/3.0); //평균 수정
+						System.out.println("수학점수가 변경되었습니다!");
+						System.out.println();
+						break;
+						
+					}//switch
+				}//if
+			}//for
+			if(count==-1) {
+				System.out.printf("※※ 찾고자 하는 %s 학생이 없습니다. ※※ 다시 입력하세요 !! \n",name);
+				System.out.println();
+			}
+		}//while
+	}//stuUpdate
+	
+	//학생성적 삭제 메소드
+    void stuDelete() {
+    	//무한반복
+		while(true) {
+			System.out.println();
+			System.out.println("[ 학생성적 삭제 ]");
+			System.out.printf("삭제하고자 하는 학생 이름을 입력하세요.(0.이전페이지 이동)\n");
+			String name = scan.next();
+			if(name.equals("0")) { //이전페이지 이동
+				System.out.println(">> 이전페이지로 이동합니다!!");
+				System.out.println();
+				break;
+			}//if
+			
+			int count = -1; //0,1,2......
+			for(int i=0;i<list.size();i++) {
+				StuScore s = (StuScore) list.get(i);
+				if(name.equals(s.getName())) {
+					count = i; //찾은 방 번호
+					System.out.printf("%s 학생을 찾았습니다.\n",name);
+					System.out.println("[ 학생성적삭제 선택 ] ");
+					System.out.println("1. 예");
+					System.out.println("2. 아니오");
+					System.out.println("------------------");
+					System.out.println("원하는 번호를 선택하세요.");
+					int choice = scan.nextInt();
+					
+					switch(choice) {
+					case 1:
+						list.remove(i);
+						System.out.printf("%s 학생성적이 삭제되었습니다! \n",name);
+						System.out.println();
+						break;
+					case 2:
+						System.out.printf("%s 학생성적이 취소되었습니다! \n",name);
+						System.out.println();
+						break;
+					}//switch
+				}//if
+			}//for
+			if(count==-1) {
+				System.out.printf("※※ 찾고자 하는 %s 학생이 없습니다. ※※ 다시 입력하세요 !! \n",name);
+				System.out.println();
+			}//if
+			
+		}//while
+    	
+    }//stuDelete
+	
 	
 	
 }//class
